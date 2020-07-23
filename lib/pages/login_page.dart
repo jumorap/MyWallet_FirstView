e797a6fa80d3dcb532c2b901089ad73d8ba6852d
Jumorap <jumorap@unal.edu.co>
2020-07-22 22:43:24 -0500

    Was added a new user preference, where can select the presentation of graphic, in lines and pie, where is separated by categories. Additionally, was added a new option of delete specific expenses for category, so, when the user do click in a specific category, this open a new layout-activity, with the history of expenses for category, sorted by day and viewed nicely whit an icon. The widget where this information appear can be slade, so, with this interaction, the user remove the specific expense in Firebase. And, additionally was built a friendly and beautiful login page, denoted like login_page.dart, with an image of free use from https://undraw.co/

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../login_state.dart';
 
@@ -7,23 +8,90 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        // Coming from the class 'login_state.dart', we generate a CircularProgressIndicator
-        // teaching to our users a loading bar circular progress
-        child: Consumer<LoginState>(
-          builder: (BuildContext context, LoginState value, Widget child) {
-            if (value.isLoading()) {
-              return CircularProgressIndicator();
-            } else {
-              return child;
-            }
-          },
-          child: RaisedButton(
-            child: Text("Ingresar"),
-            onPressed: () {
-              Provider.of<LoginState>(context).login();
-            },
-          ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Container(),
+            ),
+            Text(
+              "Mis gastos",
+              style: Theme.of(context).textTheme.display1,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Image.asset('assets/intro_image.png'),
+            ),
+            Text(
+              "Administra tus gastos",
+              style: Theme.of(context).textTheme.caption,
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(),
+            ),
+            Consumer<LoginState>(
+              builder: (BuildContext context, LoginState value, Widget child) {
+                if (value.isLoading()) return CircularProgressIndicator();
+                else return child;
+              },
+              // To build a button with an image and text, inclusive
+              // styling these, we can use 'child: OutlineButton.icon'
+              // where we use 'Text' like label.
+              //
+              // This class of button have a transparent background color
+              // and when be selected have gray color. Twin with border
+              child: OutlineButton.icon(
+                icon: Icon(FontAwesomeIcons.google),
+                label: Text(
+                  "Ingresar con Google",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18.0,
+                  ),
+                ),
+                onPressed: () {
+                  Provider.of<LoginState>(context).login();
+                },
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: Theme.of(context).textTheme.bodyText1,
+                  text: "Al ingresar, está aceptando nuestros ",
+                  children: [
+                    // We can make clickable text
+                    TextSpan(
+                      text: "Términos, Condiciones",
+                      //recognizer: _recognizer1,
+                      style: Theme.of(context)
+                        .textTheme
+                        .bodyText1
+                        .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(text: " y  "),
+                    TextSpan(
+                      text: "Políticas de Privacidad",
+                      //recognizer: _recognizer2,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText1
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                  ]
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
